0577fc288bf4a707b50bddebba22288b84503e5c
mariosandoval <mariosandoval@techtv2-2.local>
2019-07-03 14:11:03 -0500

    Swipe entre sesiones

---
+++ Prueba Login/src/IOSTest2.java
@@ -0,0 +1,49 @@
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+
+public class IOSTest2 extends Practice {
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException{
+
+		IOSDriver<IOSElement>driver = capabilities();
+		
+		
+		
+		driver.findElementByAccessibilityId("newUser").click();
+		driver.findElementByAccessibilityId("usernameTextField").click();
+		driver.findElementByAccessibilityId("usernameTextField").sendKeys("kn.alu1p2a");
+		driver.findElementByAccessibilityId("usernameTextField").sendKeys(Keys.RETURN);
+		driver.findElementByAccessibilityId("passwordTextField").click();
+		driver.findElementByAccessibilityId("passwordTextField").sendKeys("1234");
+		driver.hideKeyboard();
+		driver.findElementByAccessibilityId("loginButton").click();
+		driver.findElementByAccessibilityId("Mis Clases").click();
+		//driver.findElementByAccessibilityId("buttonScreen").click();
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, String> scrollObject = new HashMap<String, String>();
+        scrollObject.put("direction", "left");
+        js.executeScript("mobile: swipe", scrollObject);
+        scrollObject.put("direction", "left");
+        js.executeScript("mobile: swipe", scrollObject);
+        scrollObject.put("direction", "left");
+        js.executeScript("mobile: swipe", scrollObject);
+        scrollObject.put("direction", "left");
+        js.executeScript("mobile: swipe", scrollObject);
+        
+		driver.findElementByAccessibilityId("activityButton6").click();
+		driver.findElementByAccessibilityId("step2").click();
+		scrollObject.put("direction", "up");
+        js.executeScript("mobile: swipe", scrollObject);
+		
+	        
+	}
+}
+	       
+		
+		
